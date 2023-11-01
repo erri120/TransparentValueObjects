028200219e91ad367e2c468ccfe50437b80be434
erri120 <erri120@protonmail.com>
2023-11-01 19:35:37 +0100

    Cleanup

---
+++ src/TransparentValueObjects/CodeWriter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Text;
+
+namespace TransparentValueObjects;
+
+public class CodeWriter : ICodeBlock
+{
+    private readonly StringBuilder _stringBuilder = new();
+    private int _depth = 0;
+
+    public ICodeBlock AddBlock()
+    {
+        AppendLine("{");
+        _depth++;
+        return this;
+    }
+
+    public CodeWriter AppendLine(string line)
+    {
+        _stringBuilder.Append(new string('\t', _depth));
+        _stringBuilder.AppendLine(line);
+        return this;
+    }
+
+    public CodeWriter AppendLine()
+    {
+        _stringBuilder.AppendLine();
+        return this;
+    }
+
+    public override string ToString() => _stringBuilder.ToString();
+
+    public void Dispose()
+    {
+        _depth--;
+        AppendLine("}");
+        AppendLine();
+    }
+}
+
+public interface ICodeBlock : IDisposable { }
