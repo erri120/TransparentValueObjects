8d4138b887ed2fcd30814e12487f3329a6ee469b
Vitalii Mikhailov <personal@aragas.org>
2023-11-09 18:48:23 +0200

    Added docs

--- src/TransparentValueObjects.Augments/IHasRandomValueGenerator.cs
+++ src/TransparentValueObjects.Augments/IHasRandomValueGenerator.cs
@@ -2,12 +2,26 @@ using System;
 
 namespace TransparentValueObjects.Augments;
 
+/// <summary>
+/// Augment to enable support for random value generation via <see cref="Random"/>.
+/// </summary>
+/// <typeparam name="TValueObject"></typeparam>
+/// <typeparam name="TValue"></typeparam>
+/// <typeparam name="TRandom"></typeparam>
 public interface IHasRandomValueGenerator<out TValueObject, out TValue, out TRandom>
     where TValueObject : IValueObject<TValue>
     where TValue : notnull
     where TRandom : Random
 {
+    /// <summary>
+    /// Gets the random source. <see cref="Random.Shared"/> can be used for better performance.
+    /// </summary>
+    /// <returns></returns>
     public static abstract TRandom GetRandom();
 
+    /// <summary>
+    /// Gets the random object.
+    /// </summary>
+    /// <returns></returns>
     public static abstract TValueObject NewRandomValue();
 }
