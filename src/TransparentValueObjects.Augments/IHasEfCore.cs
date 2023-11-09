510bbc2e2bbb214d06b4b0b13bab27bb8655c1db
Vitalii Mikhailov <personal@aragas.org>
2023-11-09 18:43:34 +0200

    Added docs

--- src/TransparentValueObjects.Augments/IHasEfCore.cs
+++ src/TransparentValueObjects.Augments/IHasEfCore.cs
@@ -1,5 +1,13 @@
+using JetBrains.Annotations;
+
 namespace TransparentValueObjects.Augments;
 
+/// <summary>
+/// Augment to enable support for EF Core model configuration.
+/// </summary>
+/// <typeparam name="TValueObject"></typeparam>
+/// <typeparam name="TValue"></typeparam>
+[PublicAPI]
 public interface IHasEfCore<out TValueObject, out TValue>
     where TValueObject : IValueObject<TValue>
     where TValue : notnull
