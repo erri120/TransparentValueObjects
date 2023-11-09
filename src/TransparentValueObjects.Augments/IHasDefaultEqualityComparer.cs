ed6f23d7921ebf933b6dbe6b73941f98471842fe
erri120 <erri120@protonmail.com>
2023-11-09 13:01:14 +0100

    Update docs

--- src/TransparentValueObjects.Augments/IHasDefaultEqualityComparer.cs
+++ src/TransparentValueObjects.Augments/IHasDefaultEqualityComparer.cs
@@ -3,10 +3,19 @@ using JetBrains.Annotations;
 
 namespace TransparentValueObjects.Augments;
 
+/// <summary>
+/// Augment for specifying a default equality comparer.
+/// </summary>
+/// <remarks>
+/// Use this augment to provide an equality comparer that is not <see cref="EqualityComparer{T}.Default"/>.
+/// </remarks>
 [PublicAPI]
 public interface IHasDefaultEqualityComparer<TValueObject, in TValue>
     where TValueObject : IValueObject<TValue>
     where TValue : notnull
 {
+    /// <summary>
+    /// Gets the default equality comparer.
+    /// </summary>
     public static abstract IEqualityComparer<TValue> InnerValueDefaultEqualityComparer { get; }
 }
--- src/TransparentValueObjects.Augments/IHasDefaultValue.cs
+++ src/TransparentValueObjects.Augments/IHasDefaultValue.cs
@@ -2,10 +2,19 @@ using JetBrains.Annotations;
 
 namespace TransparentValueObjects.Augments;
 
+/// <summary>
+/// Augment to provide a default value for the value object.
+/// </summary>
+/// <remarks>
+/// This augment allows you to use the default constructor.
+/// </remarks>
 [PublicAPI]
 public interface IHasDefaultValue<out TValueObject, TValue>
     where TValueObject : IValueObject<TValue>
     where TValue : notnull
 {
+    /// <summary>
+    /// Gets the default value for the value object.
+    /// </summary>
     public static abstract TValueObject DefaultValue { get; }
 }
